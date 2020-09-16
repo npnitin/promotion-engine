683b1956eef98af183b85662eaf8e918a40a6feb
Nitin Ashok Pawar <npawar@altimetrik.com>
2020-09-16 20:34:10 +0530

    initialise empty project

---
+++ src/test/java/com/example/promotionenginedemo/PromotionEngineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promotionenginedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionEngineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
